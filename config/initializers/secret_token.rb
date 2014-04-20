a48e45dc4d7884687a3d2cbacfdb4b1d9b36c70b
goodbedford <bedford@goodbedford.com>
2014-04-20 04:45:00 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GbZurb::Application.config.secret_key_base = '8f3b39cd5bef468721ea0bb79427f8145fc69c41302df02c84d5e828a3ff17921cbe79c54d7fdc10db491e6a9fe94313ab9291458650630e6ad68c5046a8d961'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GbZurb::Application.config.session_store :cookie_store, key: '_gb_zurb_session'
